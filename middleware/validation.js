ec3ec04b813c79aa88b3c0dd3b9280e805203fa0
nafees-irshad <m.nafees619@gmail.com>
2024-08-21 18:11:47 +0500

    changes in validation file

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,1 +1,23 @@
-const { check, validationResult } = require('express-validator');
+const {check, validationResult} = require('express-validator');
+
+const validationSignUp = [
+    check('email')
+        .isEmail()
+        .withMessage('Please Enter Valid Email Address'),
+
+    check('password')
+        .isLength({min:6})
+        .withMessage('Password must be at least 6 characters'),
+
+    check('name')
+        .notEmpty()
+        .withMessage('Name is required')
+
+    (req, resp, next)=>{
+        const error = validationResult(req);
+        if(!error.isEmpty()){
+            return resp.status(400).json({errors: errors.array() })
+        }
+        next();
+    }
+]   
