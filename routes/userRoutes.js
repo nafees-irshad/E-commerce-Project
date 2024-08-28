9a2805587009cb3bc3c7b3c7b83babd10051675c
nafees-irshad <m.nafees619@gmail.com>
2024-08-28 19:05:22 +0500

    new changes in validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const checkUserAuth = require('../middleware/authMiddleware')
-const validationSignUp = require('../middleware/validation')
-//Import userModel 
-const {userRegistration, userLogin, changePassword, updateUser, loggedUser} = require('../controllers/userController');
+const checkUserAuth = require("../middleware/authMiddleware");
+const {
+  validateSignUp,
+  validateLogin,
+  validateChangePassword,
+} = require("../validation/validater");
+//Import userModel
+const {
+  userRegistration,
+  userLogin,
+  changePassword,
+} = require("../controllers/userController");
 
-//Route level Middleware 
-router.use('/change-password', checkUserAuth)
-router.use('/update', checkUserAuth)
-router.use('/register', validationSignUp)
-
+//Route level Middleware
+router.use("/change-password", checkUserAuth);
+// router.use("/update", checkUserAuth);
 
 //Public Routes
-router.post('/register', userRegistration);
-router.post('/login', userLogin);
+router.post("/register", validateSignUp, userRegistration);
+router.post("/login", validateLogin, userLogin);
 
-//Protected Routes 
-router.post('/change-password', changePassword)
-router.put('/update', updateUser)
-router.get('/logged-user', loggedUser)
-
-
-
+//Protected Routes
+router.post("/change-password", validateChangePassword, changePassword);
+// router.put("/update", updateUser);
+// router.get("/logged-user", loggedUser);
 
 module.exports = router;
